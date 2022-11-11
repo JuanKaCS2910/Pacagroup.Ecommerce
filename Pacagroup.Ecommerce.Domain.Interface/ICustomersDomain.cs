e0ce3222d1b23b250e803934c612830e1bfb83d9
Juan Carlos <juan.castro.socla@gmail.com>
2022-11-11 15:45:36 -0500

    Capa Domain.

---
+++ Pacagroup.Ecommerce.Domain.Interface/ICustomersDomain.cs
@@ -0,0 +1,25 @@
+﻿using Pacagroup.Ecommerce.Domain.Entity;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Pacagroup.Ecommerce.Domain.Interface
+{
+    public interface ICustomersDomain
+    {
+        #region Método Síncronos
+        bool Insert(Customers customers);
+        bool Update(Customers customers);
+        bool Delete(string customerId);
+        Customers Get(string customerId);
+        IEnumerable<Customers> GetAll();
+        #endregion
+
+        #region Método Asyncrono
+        Task<bool> InsertAsync(Customers customers);
+        Task<bool> UpdateAsync(Customers customers);
+        Task<bool> DeleteAsync(string customerId);
+        Task<Customers> GetAsync(string customerId);
+        Task<IEnumerable<Customers>> GetAllAsync();
+        #endregion
+    }
+}
