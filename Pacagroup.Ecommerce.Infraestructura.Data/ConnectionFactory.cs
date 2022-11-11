17a7c22de95e4ac7c3d79859a43168b83aef1d82
Juan Carlos <juan.castro.socla@gmail.com>
2022-11-11 15:06:57 -0500

    Agregar archivos de proyecto.

---
+++ Pacagroup.Ecommerce.Infraestructura.Data/ConnectionFactory.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.Configuration;
+using Pacagroup.Ecommerce.Transversal.Common;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Pacagroup.Ecommerce.Infraestructura.Data
+{
+    public class ConnectionFactory : IConnectionFactory
+    {
+        public readonly IConfiguration _configuration;
+
+        public ConnectionFactory(IConfiguration configuration)
+        {
+            this._configuration = configuration;
+        }
+
+        public IDbConnection GetConnection
+        {
+            get
+            {
+                var sqlConnection = new SqlConnection();
+                if (sqlConnection == null) return null;
+
+                sqlConnection.ConnectionString = _configuration.GetConnectionString("NorthwindConnection");
+                sqlConnection.Open();
+                return sqlConnection;
+
+            }
+        }
+    }
+}
