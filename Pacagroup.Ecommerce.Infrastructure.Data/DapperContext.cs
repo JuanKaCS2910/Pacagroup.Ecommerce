7ee49e027945513765b0a90454cf2fd2839409e9
Juan Carlos <juan.castro.socla@gmail.com>
2022-11-30 18:29:58 -0500

    Patrón Generic Repository

---
+++ Pacagroup.Ecommerce.Infrastructure.Data/DapperContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Pacagroup.Ecommerce.Infrastructure.Data
+{
+    public class DapperContext
+    {
+        private readonly IConfiguration _configuration;
+        private readonly string _connectionString;
+
+        public DapperContext(IConfiguration configuration)
+        {
+            this._configuration = configuration;
+            this._connectionString = configuration.GetConnectionString("NorthwindConnection");
+        }
+
+        public IDbConnection CreateConnection() => new SqlConnection(this._connectionString);
+
+    }
+}
