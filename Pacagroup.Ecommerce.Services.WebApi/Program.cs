38eda4209b361f13da34389004fdd9f883839d1a
Juan Carlos <juan.castro.socla@gmail.com>
2022-12-01 17:13:45 -0500

    Refactorizando .NET 6.0

--- Pacagroup.Ecommerce.Services.WebApi/Program.cs
+++ Pacagroup.Ecommerce.Services.WebApi/Program.cs
@@ -1,20 +1,61 @@
-﻿using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Hosting;
+﻿#region Region Using
+using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Mvc.ApiExplorer;
+using Pacagroup.Ecommerce.Services.WebApi.Modules.Authentication;
+using Pacagroup.Ecommerce.Services.WebApi.Modules.Feature;
+using Pacagroup.Ecommerce.Services.WebApi.Modules.HealthCheck;
+using Pacagroup.Ecommerce.Services.WebApi.Modules.Injection;
+using Pacagroup.Ecommerce.Services.WebApi.Modules.Mapper;
+using Pacagroup.Ecommerce.Services.WebApi.Modules.Swagger;
+using Pacagroup.Ecommerce.Services.WebApi.Modules.Validator;
+using Pacagroup.Ecommerce.Services.WebApi.Modules.Versioning;
+#endregion
 
-namespace Pacagroup.Ecommerce.Services.WebApi
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddApiEndpointHttpClient();
+
+builder.Services.AddMapper();//Mapper Extensiones
+builder.Services.AddFeature(builder.Configuration);//Feature Extensiones
+builder.Services.AddInjection(builder.Configuration);//Injection Extensiones
+builder.Services.AddAuthentication(builder.Configuration);//Authentication Extensiones
+builder.Services.AddVersioning(); // Versioning Extensiones
+builder.Services.AddSwagger();//SwaggerExtensiones
+builder.Services.AddValidator();
+builder.Services.AddHealthCheck(builder.Configuration);
+
+var app = builder.Build();
+
+//Configure the Http request pipeline
+if (app.Environment.IsDevelopment())
 {
-    public class Program
+    app.UseDeveloperExceptionPage();
+    app.UseSwagger();
+    // specifying the Swagger JSON endpoint.
+    app.UseSwaggerUI(c =>
     {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
-    }
+        //build a swagger endpoint for each discovered API Version
+        var provider = app.Services.GetRequiredService<IApiVersionDescriptionProvider>();
+        foreach (var description in provider.ApiVersionDescriptions)
+        {
+            c.SwaggerEndpoint($"/swagger/{description.GroupName}/swagger.json", description.GroupName.ToUpperInvariant());
+        }
+    });
 }
+
+app.UseHttpsRedirection();
+app.UseCors("policyApiEcommerce");
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+app.MapHealthChecksUI();
+app.MapHealthChecks("/health", new Microsoft.AspNetCore.Diagnostics.HealthChecks.HealthCheckOptions
+{
+    Predicate = _ => true, //En caso desee agregar filtros.
+    ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+});
+
+app.Run();
