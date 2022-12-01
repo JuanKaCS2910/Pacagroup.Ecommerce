9035975b11e2f48209d149003b1b4368699a1e38
Juan Carlos <juan.castro.socla@gmail.com>
2022-11-30 19:02:27 -0500

    Patrón UnitOfWork

---
+++ Pacagroup.Ecommerce.Infrastructure.Interface/IUnitOfWork.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Pacagroup.Ecommerce.Infrastructure.Interface
+{
+    public interface IUnitOfWork : IDisposable //Permite liberar recursos en memoria.
+    {
+        ICustomersRepository Customers { get; }
+        IUsersRepository Users { get; } 
+    }
+}
