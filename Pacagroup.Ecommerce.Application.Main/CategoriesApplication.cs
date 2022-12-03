f97100276098c99dc7b14be65b90f9137d763812
Juan Carlos <juan.castro.socla@gmail.com>
2022-12-03 18:47:42 -0500

    Subida Final

--- Pacagroup.Ecommerce.Application.Main/CategoriesApplication.cs
+++ Pacagroup.Ecommerce.Application.Main/CategoriesApplication.cs
@@ -1,5 +1,6 @@
 ﻿#region Region Using
 using AutoMapper;
+using Microsoft.Extensions.Caching.Distributed;
 using Pacagroup.Ecommerce.Application.DTO;
 using Pacagroup.Ecommerce.Application.Interface;
 using Pacagroup.Ecommerce.Domain.Interface;
@@ -7,6 +8,7 @@ using Pacagroup.Ecommerce.Transversal.Common;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 #endregion
 
@@ -17,21 +19,40 @@ namespace Pacagroup.Ecommerce.Application.Main
         private readonly ICategoriesDomain _categoriesDomain;
         private readonly IMapper _mapper;
         private readonly IAppLogger<CategoriesApplication> _logger;
+        private readonly IDistributedCache _distributedCache;
 
-        public CategoriesApplication(ICategoriesDomain categoriesDomain, IMapper mapper, IAppLogger<CategoriesApplication> logger)
+        public CategoriesApplication(ICategoriesDomain categoriesDomain, IMapper mapper, IAppLogger<CategoriesApplication> logger, IDistributedCache distributedCache)
         {
             _categoriesDomain = categoriesDomain;
             _mapper = mapper;
             _logger = logger;   
-        }
+            _distributedCache = distributedCache;
+    }
 
         public async Task<Response<IEnumerable<CategoriesDto>>> GetAll()
         {
             var response = new Response<IEnumerable<CategoriesDto>>();
+            var cacheKey = "categoriesList";
             try
             {
-                var categories = await _categoriesDomain.GetAll();
-                response.Data = _mapper.Map<IEnumerable<CategoriesDto>>(categories);
+                var redisCategories = await _distributedCache.GetAsync(cacheKey);
+                if (redisCategories != null)
+                    response.Data = JsonSerializer.Deserialize<IEnumerable<CategoriesDto>>(redisCategories); 
+                else
+                {
+                    var categories = await _categoriesDomain.GetAll();
+                    response.Data = _mapper.Map<IEnumerable<CategoriesDto>>(categories);
+                    if (response.Data != null)
+                    {
+                        var serializedCategories = Encoding.UTF8.GetBytes(JsonSerializer.Serialize(response.Data));
+                        var options = new DistributedCacheEntryOptions()
+                            .SetAbsoluteExpiration(DateTime.Now.AddHours(8))
+                            .SetSlidingExpiration(TimeSpan.FromMinutes(60)); // Si nadie accede a cache a consultar 60 minutos caduca de manera automática
+
+                        await _distributedCache.SetAsync(cacheKey, serializedCategories, options);
+                    }
+                }
+
                 if (response.Data != null)
                 {
                     response.IsSuccess = true;
